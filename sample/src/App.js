10641d3f2265fbbccedc69f04db69f66968c982b
jinsil <developer_corn@daum.net>
2022-08-21 00:07:17 +0900

    test MUI

--- sample/src/App.js
+++ sample/src/App.js
@@ -1,32 +1,10 @@
-//import logo from './logo.svg';
-//import './App.css';
-import { useState } from 'react';
+import './App.css';
+import Button from '@mui/material/Button';
 
-function Header(props) {
-  return <header>
-    <h1><a href="/" onClick={(event) => {
-      event.preventDefault();
-      props.onChangeMode();
-    }}>{props.title}</a></h1>
-  </header>
-}
-function Nav(props) {
-  const lis = [];
-  for (let i = 0; i < props.topics.length; i++) {
-    let t = props.topics[i];
-    lis.push(<li key={t.id}>
-      <a id={t.id} href={'/read' + t.id} onClick={(event) => {
-        event.preventDefault();
-        props.onChangeMode(Number(event.target.id));
-      }}>{t.title}</a>
-    </li>)
-  }
-  return <nav>
-    <ol>
-      {lis}
-    </ol>
-  </nav>
-}
+import { useState } from 'react';
+import Header from './Header.js';
+import Nav from './Nav.js';
+
 function Article(props) {
   return <article>
     <h2>{props.title}</h2>
@@ -100,7 +78,7 @@ function App() {
         e.preventDefault();
         setMode('UPDATE');
       }}>Update</a></li>
-      <li><input type="button" value="delete" onClick={() => {
+      <li><Button variant="outlined" onClick={() => {
         const newTopics = []
         for (let i = 0; i < topics.length; i++) {
           if (topics[i].id !== id) {
@@ -108,7 +86,7 @@ function App() {
           }
         }
         setTopics(newTopics);
-      }} /></li>
+      }}>Delete</Button></li>
     </>
   } else if (mode === 'CREATE') {
     content = <Create onCreate={(_title, _body) => {
