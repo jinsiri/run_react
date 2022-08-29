04d3f25b191a0b502e65d44bd4d7a7bc8448b1b4
jinsil <developer_corn@daum.net>
2022-08-30 06:31:41 +0900

    react hook

--- classandfunc/src/App.js
+++ classandfunc/src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-function FuncComp(props) {
+function App() {
     return (
-      <div className="container">
-          <h2>function style component</h2>
-          <p>Number : {props.initNumber}</p> // 클래스와 차이점 체크.
-      </div>
+        <div className="App container">
+            <h1>TEST</h1>
+            <FuncComp initNumber={2}></FuncComp>
+            <ClassComp initNumber={2}></ClassComp>
+        </div>
+    );
+}
+
+function FuncComp(props) {
+    let numberState = useState(props.initNumber);
+    let number = numberState[0];
+    let setNumber = numberState[1];
+    return (
+        <div className="container">
+            <h2>function style component</h2>
+            <p>Number : {number}</p> // 클래스와 차이점 체크.
+            <input type="button" value="random" onClick={
+                function () {
+                    setNumber(Math.random());
+                }
+            } />
+        </div>
     );
 } //자기 자신이 render 함수, 원래 간단한 기능만 function으로 썼었음
 
@@ -21,23 +39,13 @@ class ClassComp extends React.Component {
                 {/* <p>Number : {this.props.initNumber}</p> // 받는 법 차이 체크. */}
                 <p>Number : {this.state.number}</p>
                 <input type="button" value="random" onClick={
-                    function(){
-                        this.setState({number:Math.random()})
+                    function () {
+                        this.setState({ number: Math.random() })
                     }.bind(this)
                 } /> {/* 클래스 방식의 안 좋은 점 중 하나, bind(this)를 통해야함 */}
             </div>
         );
     }
-}
-
-function App() {
-  return (
-    <div className="App container">
-        <h1>TEST</h1>
-        <FuncComp initNumber={2}></FuncComp>
-        <ClassComp initNumber={2}></ClassComp>
-    </div>
-  );
 }
 
 export default App;
