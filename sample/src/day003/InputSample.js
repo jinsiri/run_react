baad6a321305436b26135cda42b5919f79a07143
jinsil <developer_corn@daum.net>
2022-09-02 23:34:07 +0900

    여러개 인풋 상태 변경

--- sample/src/day003/InputSample.js
+++ sample/src/day003/InputSample.js
@@ -1,22 +1,35 @@
 import React, { useState } from 'react';
 
 function InputSample() {
-    const [value, setValue] = useState('');
+    const [inputs, setInputs] = useState({
+        name: '',
+        nickname: ''
+    }); // 초기값
+
+    const { name, nickname } = inputs; // 비구조화 할당으로 값 추출
 
     const inputChange = (e) => {
-        setValue(e.target.value);
+        const { value, name } = e.target; // e.target에서 name과 value 추출
+        setInputs({
+            ...inputs,
+            [name]: value // name키 값 가진 값 value로 설정
+        });
     }
 
     const resetValue = () => {
-        setValue('');
+        setInputs({
+            name: '',
+            nickname: '',
+        })
     }
 
     return (
         <div>
-            <input value={value} onChange={inputChange} />
+            <input name="name" value={name} onChange={inputChange} />
+            <input name="nickname" value={nickname} onChange={inputChange} />
             <button onClick={resetValue}>초기화</button>
             <div>
-                <b>값: {value}</b>
+                <b>{name}: {nickname}</b>
             </div>
         </div>
     );
