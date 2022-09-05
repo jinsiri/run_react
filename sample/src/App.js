b1387ba79cc9e2ddb263766b4056411b6d9a2e45
jinsil <developer_corn@daum.net>
2022-09-06 07:02:40 +0900

    useMemo, useCallback

--- sample/src/App.js
+++ sample/src/App.js
@@ -1,7 +1,12 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useMemo } from 'react';
 import './App.css';
 import CreateUser from './day004/CreateUser';
 import UserList from './day004/UserList';
+
+function countActiveUsers(users) {
+  console.log('활성 사용자 수 세는 중...');
+  return users.filter(user => user.active).length;
+}
 
 function App() {
   // 입력 받는 인풋 관리
@@ -72,6 +77,13 @@ function App() {
     )
   }
 
+  const count = useMemo(() => countActiveUsers(users), [users]);
+  // useMemo 의 첫번째 파라미터에는 어떻게 연산할지 정의하는 함수를 넣어주면 되고 
+  // 두번째 파라미터에는 deps 배열을 넣어주면 되는데, 이 배열 안에 넣은 내용이 바뀌면 등록한 함수를 호출해서 값을 연산해주고, 
+  // 내용이 바뀌지 않았다면 이전에 연산한 값을 재사용
+
+  // useMemo 는 특정 결과값을 재사용 할 때 사용하는 반면, useCallback 은 특정 함수를 새로 만들지 않고 재사용하고 싶을때 사용
+
   return (
     <div className="App">
       <CreateUser
@@ -81,6 +93,7 @@ function App() {
         onCreate={onCreate}
       ></CreateUser>
       <UserList users={users} onRemove={onRemove} onToggle={onToggle}></UserList>
+      <div>활성사용자 수 : {count}</div>
     </div>
   );
 }
